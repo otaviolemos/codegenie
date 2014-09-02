38b1bcf2a756f5ea4bb453b6a7d26163730feda0
adrianocarv <adrianocarv@gmail.com>
2014-09-02 15:04:22 +0000

    Priorize original term on Query

---
+++ sourcerer-aqe/test/br/unifesp/ppgcc/sourcereraqe/infrastructure/SourcererQueryBuilderTest.java
@@ -0,0 +1,11 @@
+package br.unifesp.ppgcc.sourcereraqe.infrastructure;
+
+import org.junit.Test;
+
+public class SourcererQueryBuilderTest {
+
+	@Test
+	public void prioritizeOrininalTermsTest(){
+		
+	}
+}
